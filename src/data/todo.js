bd8ed75b5dc3b8c3cc87c79c758b2c78b32e5fd2
Rebecca Martin <rebecca-martin@live.com>
2018-08-09 12:02:38 -0700

    added dummy data

---
+++ src/data/todo.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, milk, bread',
+        _id: '001'
+    },
+
+    {
+        title: 'Wash car',
+        details: 'Get car detailed and waxed',
+        _id: '002'
+    },
+
+    {
+        title: 'Pick up kids',
+        details: 'Get kids from soccer practice',
+        _id: '003'
+    },
+
+    {
+        title: 'Gym',
+        details: 'One hour of cardio',
+        _id: '004'
+    },
+
+    {
+        title: 'Home Depot',
+        details: 'Pick up screws from Home Depot',
+        _id: '005'
+    },
+
+    {
+        title: 'Laundry',
+        details: 'Bring in dry cleaning',
+        _id: '006'
+    }
+];
+
