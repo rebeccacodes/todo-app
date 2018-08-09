f71a08fd38fbbf2f2bfa64ce8ee3e39a0d0fb6c2
Rebecca Martin <rebecca-martin@live.com>
2018-08-09 15:40:25 -0700

    finished add item functionality

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import listData from '../data/todo';
+
 
 class ToDoList extends Component {
     render() {
-        const listElements = listData.map(item => {
+        const listElements = this.props.list.map(item => {
             return <li className="collection-item" key={item._id}>{item.title}</li>
         });
         return (
